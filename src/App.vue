4557e59dc01155b09c7985ba2bbc17549cfe08f2
Omar Agha <omar.autotech-co.com>
2025-11-19 00:37:36 +0400

    create navbar re-useable component.

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from "./components/navbar/Navbar.vue";
+</script>
+
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <v-app-bar density="comfortable" color="primary" dark>
+    <v-app-bar-title>Fleet Tracking Dashboard</v-app-bar-title>
+    <v-spacer />
+    <v-btn to="/" variant="text">Live Map</v-btn>
+    <v-btn to="/vehicles" variant="text">Vehicle List</v-btn>
+  </v-app-bar>
+</template>
