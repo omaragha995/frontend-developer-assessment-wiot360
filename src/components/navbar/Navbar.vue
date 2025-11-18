966580d6ddf570665a3634705dfeb3cfbd8041f2
Omar Agha <omar.autotech-co.com>
2025-11-19 01:32:47 +0400

    add router-link.

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -2,7 +2,11 @@
 
 <template>
   <v-app-bar density="comfortable" color="primary" dark>
-    <v-app-bar-title>Fleet Tracking Dashboard</v-app-bar-title>
+    <v-app-bar-title>
+      <router-link to="/" custom v-slot="{ navigate }">
+        <button @click="navigate">Fleet Tracking Dashboard</button>
+      </router-link>
+    </v-app-bar-title>
     <v-spacer />
     <v-btn to="/" variant="text">Live Map</v-btn>
     <v-btn to="/vehicles" variant="text">Vehicle List</v-btn>
